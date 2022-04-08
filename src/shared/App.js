be98f8703655ba9fb731aeb9896ae76cc5071865
MiriGu <miriggu9@gmail.com>
2022-04-08 10:08:53 +0900

    Notification 예외처리

--- src/shared/App.js
+++ src/shared/App.js
@@ -36,32 +36,34 @@ import ScreenBackground from "../components/ScreenBackground/ScreenBackground";
 import "./App.css";
 
 function App() {
-
   const dispatch = useDispatch();
 
-  Notification.requestPermission().then((status) => {
+  const isNotification = "Notification" in window;
 
-    if (status === "denied") {
-      alert("Notification 거부됨");
-    } else if (navigator.serviceWorker) {
-      navigator.serviceWorker
-        .register("/pwabuilder-sw.js") // serviceworker 등록
-        .then(function (registration) {
-          const subscribeOptions = {
-            userVisibleOnly: true,
-            // push subscription이 유저에게 항상 보이는지 여부. 알림을 숨기는 등 작업이 들어가지는에 대한 여부인데, 크롬에서는 true 밖에 지원안한다.
-            // https://developers.google.com/web/fundamentals/push-notifications/subscribing-a-user
-            applicationServerKey: process.env.REACT_APP_APPLICATIONSERVERKEY, // 발급받은 vapid public key
-          };
+  if (isNotification) {
+    Notification.requestPermission().then((status) => {
+      if (status === "denied") {
+        alert("Notification 거부됨");
+      } else if (navigator.serviceWorker) {
+        navigator.serviceWorker
+          .register("/pwabuilder-sw.js") // serviceworker 등록
+          .then(function (registration) {
+            const subscribeOptions = {
+              userVisibleOnly: true,
+              // push subscription이 유저에게 항상 보이는지 여부. 알림을 숨기는 등 작업이 들어가지는에 대한 여부인데, 크롬에서는 true 밖에 지원안한다.
+              // https://developers.google.com/web/fundamentals/push-notifications/subscribing-a-user
+              applicationServerKey: process.env.REACT_APP_APPLICATIONSERVERKEY, // 발급받은 vapid public key
+            };
 
-          return registration.pushManager.subscribe(subscribeOptions);
-        })
-        .then(function (pushSubscription) {
-          // subscription 정보를 저장할 서버로 보낸다.
-          dispatch(userActions.subSetDB(pushSubscription));
-        });
-    }
-  });
+            return registration.pushManager.subscribe(subscribeOptions);
+          })
+          .then(function (pushSubscription) {
+            // subscription 정보를 저장할 서버로 보낸다.
+            dispatch(userActions.subSetDB(pushSubscription));
+          });
+      }
+    });
+  }
 
   React.useEffect(() => {
     const userId = localStorage.getItem("userId");
